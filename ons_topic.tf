a59c341cd3340187221bd9321424f60a429e169f
Nuruddin Ahmed <nuruddin.ahmed@oracle.com>
2024-10-21 18:34:13 -0500

    Fixed again

--- ons_topic.tf
+++ ons_topic.tf
@@ -12,9 +12,14 @@ output "notification_topic_ocid" {
   value = oci_ons_notification_topic.grafana_alerts.id
 }
 
-resource "null_resource" "write_topic_ocid" {
+# Use local_file to write the OCID to a file
+resource "local_file" "write_topic_ocid" {
+  filename = "$HOME/topic_ocid.txt"  # You can replace this with an absolute path if needed
+  content  = "${oci_ons_notification_topic.grafana_alerts.id}"  # Write the OCID to the file
+
+  # Optionally, set permissions after file creation (manual step if necessary)
   provisioner "local-exec" {
-    command = "echo ${oci_ons_notification_topic.grafana_alerts.id} > $HOME/topic_ocid.txt && chmod 600 $HOME/topic_ocid.txt"
+    command = "chmod 600 $HOME/topic_ocid.txt"
   }
 
   depends_on = [oci_ons_notification_topic.grafana_alerts]
