7b31a8a8503b4cef0ff1077ecce99ff946a32d7d
Nuruddin Ahmed <nuruddin.ahmed@oracle.com>
2024-10-22 16:35:37 -0500

    Removed provisioner

--- ons_topic.tf
+++ ons_topic.tf
@@ -15,9 +15,5 @@ output "notification_topic_ocid" {
 resource "local_file" "write_topic_ocid" {
   filename = "/tmp/topic_ocid.txt"
   content  = "${oci_ons_notification_topic.grafana_alerts.id}"
-
-  provisioner "local-exec" {
-    command = "chmod 644 /tmp/topic_ocid.txt"
-  }
   depends_on = [oci_ons_notification_topic.grafana_alerts]
 }
