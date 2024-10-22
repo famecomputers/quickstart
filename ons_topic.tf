abfbdadffea5e3748fab6b252ae0e932bf4f1af8
Nuruddin Ahmed <nuruddin.ahmed@oracle.com>
2024-10-21 22:26:38 -0500

    Using environment variable to write topic ocid

--- ons_topic.tf
+++ ons_topic.tf
@@ -12,14 +12,16 @@ output "notification_topic_ocid" {
   value = oci_ons_notification_topic.grafana_alerts.id
 }
 
-# Use local_file to write the OCID to a file
-resource "local_file" "write_topic_ocid" {
-  filename = "$HOME/topic_ocid.txt"  # You can replace this with an absolute path if needed
-  content  = "${oci_ons_notification_topic.grafana_alerts.id}"  # Write the OCID to the file
-
-  # Optionally, set permissions after file creation (manual step if necessary)
+# Use local-exec to set an environment variable and write the OCID to a file
+resource "null_resource" "write_topic_ocid" {
   provisioner "local-exec" {
-    command = "chmod 600 $HOME/topic_ocid.txt"
+    # Set environment variable for the topic OCID
+    environment = {
+      TOPIC_OCID = "${oci_ons_notification_topic.grafana_alerts.id}"
+    }
+
+    # Use the environment variable to write the OCID to a file
+    command = "echo $TOPIC_OCID > $HOME/topic_ocid.txt && chmod 600 $HOME/topic_ocid.txt"
   }
 
   depends_on = [oci_ons_notification_topic.grafana_alerts]
